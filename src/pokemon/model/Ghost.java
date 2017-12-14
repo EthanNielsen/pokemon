6112b581b8be2e160c71bf5f0c32bde0de82827a
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-14 13:36:21 -0700

    added more interfaces with type Psychic Ghost and Null.

---
+++ src/pokemon/model/Ghost.java
@@ -0,0 +1,8 @@
+package pokemon.model;
+
+public interface Ghost 
+{
+	public int actionHex();
+	public boolean actionCurse(int amount);
+	public void actionAstonish();
+}
---
+++ src/pokemon/model/Psychic.java
@@ -0,0 +1,8 @@
+package pokemon.model;
+
+public interface Psychic 
+{
+	public boolean actionConfusion();
+	public void actionGravity();
+	public String actionKinesis(int amount);
+}
