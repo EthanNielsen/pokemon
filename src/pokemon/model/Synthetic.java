02d0df20997ce61945164163ea8032a405ff7d28
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-14 13:53:10 -0700

    Added Synthetic and started Mewtwo.

---
+++ src/pokemon/model/Synthetic.java
@@ -0,0 +1,26 @@
+package pokemon.model;
+
+public class Synthetic extends Pokemon implements Null
+{
+	
+	public Synthetic(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public void crushClaw()
+	{
+		
+	}
+	
+	public boolean punishmentCode(int amount)
+	{
+		return false;
+	}
+	
+	public String takeDown()
+	{
+		return "I'll take down your code with me....";
+	}
+	
+}
