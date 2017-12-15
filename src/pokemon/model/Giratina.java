14011803af42d485c37fb88e98ce153634e402c5
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-15 13:32:37 -0700

    Added Giratina Pokemon as a class.

---
+++ src/pokemon/model/Giratina.java
@@ -0,0 +1,26 @@
+package pokemon.model;
+
+public class Giratina extends Pokemon implements Ghost
+{
+
+	public Giratina(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public int actionHex()
+	{
+		return 0;
+	}
+	
+	public boolean actionCurse(int amount)
+	{
+		return true;
+	}
+	
+	public void actionAstonish()
+	{
+		
+	}
+	
+}
