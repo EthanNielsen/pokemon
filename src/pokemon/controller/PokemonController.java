6fb8de8daed4c2b0745072c7c4c4bbf02067455d
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-01-08 11:55:32 -0700

    Insterted my pokemon into the pokedex list.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,10 +1,16 @@
 package pokemon.controller;
 
 
-import pokemon.model.Pokemon;
-import java.util.List;
 import java.util.ArrayList;
-import pokemon.view.PokemonFrame;
+import java.util.List;
+
+import pokemon.model.Giratina;
+import pokemon.model.Mewtwo;
+import pokemon.model.MrMime;
+import pokemon.model.Palossand;
+import pokemon.model.Pokemon;
+import pokemon.model.Silvally;
+import pokemon.model.Synthetic;
 
 
 public class PokemonController 
@@ -13,9 +19,30 @@ public class PokemonController
 	
 	public List<Pokemon> getPokedex()
 	{
-		return pokedex;
+		pokedex = new ArrayList<Pokemon>();
+		buildPokedex();
+		
+	// Below this is doing the same as buildPokedex but instead its turning the pokemon into variables. buildPokedex adds the pokemon to the list.
+//		Giratina myGira = new Giratina();
+//		Mewtwo myMew = new Mewtwo();
+//		MrMime myMime = new MrMime();
+//		Palossand mySand = new Palossand();
+//		Silvally mySilv = new Silvally();
+//		Synthetic mySynth = new Synthetic();
+//		
+		return getPokedex();
+//		appFrame = new PokemonFrame(this);
 	}
 	
+	private void buildPokedex()
+	{
+		pokedex.add(new Giratina());
+		pokedex.add(new Mewtwo());
+		pokedex.add(new MrMime());
+		pokedex.add(new Palossand());
+		pokedex.add(new Silvally());
+		pokedex.add(new Synthetic());
+	}
 	public boolean isValidInteger(String input)
 	{
 		return false;
