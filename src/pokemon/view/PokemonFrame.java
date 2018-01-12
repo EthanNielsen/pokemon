06694393b4d84c619a95ca19f1a6767ee837a64d
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-01-12 13:13:54 -0700

    currently trying to create the Frame.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,17 @@
 package pokemon.view;
 
+import pokemon.controller.PokemonController;
+
 public class PokemonFrame 
 {
-
+	private PokemonController pokemonController;
+	private PokedexPanel pokemonPanel;
+	
+	
+	public PokemonFrame(PokemonController pokemonController)
+	{
+		super();
+		this.pokemonController = pokemonController;
+		
+	}
 }
